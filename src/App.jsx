728a155183863320506284090aba4d037a2c8756
mdmonir-hossain <monirsoftengr@gmail.com>
2024-03-30 21:48:38 +0600

    Add task form create

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TaskInput from './Components/TaskInput/TaskInput'
+import TaskList from './Components/TaskList/TaskList'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      
-    </>
+    <div>
+      <TaskInput />
+      <TaskList />
+    </div>
   )
 }
 
--- src/Components/TaskInput/TaskInput.jsx
+++ src/Components/TaskInput/TaskInput.jsx
@@ -1,9 +1,42 @@
 const TaskInput = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Task Title</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Task Title"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Task Description</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Add Task</button>
+              </div>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default TaskInput;
